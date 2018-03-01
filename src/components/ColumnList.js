f33971b597c7abd4f1c8544a179b11efc2ec33a4
Tiffany DeVine <tiffany.devine@icloud.com>
2018-02-28 18:04:15 -0800

    Scaffold for ColumnList component

--- src/components/ColumnList.js
+++ src/components/ColumnList.js
@@ -1,26 +1,28 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class extends Component {
-  render () {
+  render() {
     return (
-      <div className='list__container'>
-        <div className='list__header'>
-          <div className='list__header-title'>Do we have hot reloading?</div>
-          <div className='card-container'>
-            <ul className='cards'>
-              <li className='card-preview' />
-              <li className='card-preview' />
-              <li className='card-preview' />
+      <div className="list__container">
+        <div className="list__header">
+          <div className="list__header-title">
+            This is the first Geckos-13 list
+          </div>
+          <div className="card-container">
+            <ul className="cards">
+              <li className="card-preview" />
+              <li className="card-preview" />
+              <li className="card-preview" />
             </ul>
           </div>
-          <div className='list__add-cards'>
-            <button className='btn--add'>ADD</button>
-            <a href='' className='btn--cancel'>
+          <div className="list__add-cards">
+            <button className="btn--add">ADD</button>
+            <a href="" className="btn--cancel">
               X
             </a>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
