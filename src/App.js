7db42ee3782c02136a519c804acf53dc8880696c
Tiffany DeVine <tiffany.devine@icloud.com>
2018-03-01 23:51:59 -0800

    Renamed component List

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
-import ColumnList from './components/ColumnList'
+import List from './components/List'
 
 class App extends Component {
-  render () {
+  render() {
     return (
       <div>
-        <ColumnList />
+        <List />
       </div>
     )
   }
