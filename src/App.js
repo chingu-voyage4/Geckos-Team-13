f685e15db35fe43592ebe503e4927e8ca4ada7be
zealousAnemone <zealous.anemone@gmail.com>
2018-03-01 21:54:36 -0500

    First go at headers for board, with some CSS styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Navbar from './navbar.js';
+import NavbarTwo from './navbarTwo.js';
 
 class App extends Component {
     render() {
         return (
             <div className="App">
                 <Navbar />
+                <NavbarTwo />
             </div>
         );
     }
--- src/navbar.js
+++ src/navbar.js
@@ -4,22 +4,22 @@ import './navbar.css';
 class Navbar extends Component {
     render() {
         return (
-            <ul className = "navbar">
+            <ul className = "navbar blue-header">
                 <span className = "leftbar">
-                    <li><button className="boards"><i class="fab fa-trello"></i> Boards</button></li>
+                    <li><button className="boards"><i className="fab fa-trello"></i> Boards</button></li>
                     <span className="search">
                         <li><input type="text"></input></li>
-                        <i class="fas fa-search"></i>
+                        <i className="fas fa-search"></i>
                     </span>
                 </span>
-                <li className="brand"><i class="fab fa-trello"></i> Trello</li>
+                <li className="brand"><i className="fab fa-trello"></i> Trello</li>
                 <span className = "rightbar">
-                    <li><button className="create"><i class="fas fa-plus"></i>
+                    <li><button className="create"><i className="fas fa-plus"></i>
 
                     </button></li>
-                    <li><button className="info"><i class="fas fa-info-circle"></i></button></li>
-                    <li><button className="notifications"><i class="fas fa-bell"></i></button></li>
-                    <li><button className="userbadge"><i class="fas fa-user"></i></button></li>
+                    <li><button className="info"><i className="fas fa-info-circle"></i></button></li>
+                    <li><button className="notifications"><i className="fas fa-bell"></i></button></li>
+                    <li><button className="userbadge"><i className="fas fa-user"></i></button></li>
                 </span>
             </ul>
             
