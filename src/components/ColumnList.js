8654d494fb6446c699287ac7ea56714915ca1951
Tiffany DeVine <tiffany.devine@icloud.com>
2018-03-01 17:08:09 -0800

    Changed expanded component to form instead of a div

--- src/components/ColumnList.js
+++ src/components/ColumnList.js
@@ -4,7 +4,10 @@ export default class extends Component {
   constructor(props) {
     super(props)
 
-    this.state = { outComponentSelected: true }
+    this.state = {
+      outComponentSelected: true,
+      listTitle: "The Gecko-13s first list"
+    }
 
     this.setWrapperRef = this.setWrapperRef.bind(this)
     this.handleClickOutside = this.handleClickOutside.bind(this)
@@ -34,6 +37,7 @@ export default class extends Component {
   }
 
   handleClick(event) {
+    console.log(this.state.listTitle)
     this.setState({ outComponentSelected: false })
   }
 
@@ -48,20 +52,21 @@ export default class extends Component {
         {this.props.children}
         <div className='list__header'>
           <div className='list__header-title'>
-            This is the first Geckos-13 list lkjlkjkj
+            <input type="text" />
           </div>
         </div>
         <div className='card-container'>
           <ul className='cards'>
-            <li className='card-preview' />
-            <li className='card-preview' />
-            <li className='card-preview' />
+            <li className='card-preview'>Card component 1</li>
+            <li className='card-preview'>Card component 2</li>
+            <li className='card-preview'>Card component 3</li>
           </ul>
         </div>
-        <div ref={this.setWrapperRef} className='list__add-cards-full' style={{ display: this.state.outComponentSelected ? 'none' : 'block' }}>
+        <form ref={this.setWrapperRef} className='list__add-cards-full' style={{ display: this.state.outComponentSelected ? 'none' : 'block' }}>
+          <textarea name="card-title" id="" cols="30" rows="10"></textarea>
           <button className='btn--add'>ADD</button>
           <button onClick={this.cancelExpansion} className='btn--cancel'>X</button>
-        </div>
+        </form>
         <button onClick={this.handleClick} className="list__add-cards-short" style={{ display: this.state.outComponentSelected ? 'block' : 'none' }}>Add a card...</button>
       </div>
     )
