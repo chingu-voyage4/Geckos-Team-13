8736d12e98105de5b3b398a8040842087fa77a00
Tiffany DeVine <tiffany.devine@icloud.com>
2018-03-05 20:46:45 -0800

    Rendered List in app component

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,20 @@
-import React, { Component } from 'react'
-import './App.css'
-import Navbar from './Navbar.js'
-import NavbarTwo from './NavbarTwo.js'
-import List from './List'
+import React, { Component } from 'react';
+import '../styles/App.css';
+import NavbarTop from './NavbarTop.js';
+import NavbarBottom from './NavbarBottom.js';
+import List from './List';
+
 
 class App extends Component {
-  render () {
-    return (
-      <div className='App'>
-        <Navbar />
-        <NavbarTwo />
-        <List />
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div className="App">
+                <NavbarTop />
+                <NavbarBottom />
+                <List />
+            </div>
+        );
+    }
 }
 
 export default App
