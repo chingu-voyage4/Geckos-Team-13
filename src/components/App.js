7af911b9b6ebc37e82093d285a1b24ff1649d1f2
Tiffany DeVine <tiffany.devine@icloud.com>
2018-03-13 17:47:06 -0700

    Created Board Outline

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import '../styles/App.css'
 import NavbarTop from './NavbarTop.js'
 import NavbarBottom from './NavbarBottom.js'
-import List from './List'
+import Board from './Board'
 
 class App extends Component {
   render() {
@@ -10,7 +10,7 @@ class App extends Component {
       <div className='App'>
         <NavbarTop />
         <NavbarBottom />
-        <List />
+        <Board />
       </div>
     )
   }
