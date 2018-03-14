28a9b70b11f83722c08c054204fbbd4ba2aca144
Tiffany DeVine <tiffany.devine@icloud.com>
2018-03-13 17:05:37 -0700

    Removed Card popup from Board View

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,6 @@ import '../styles/App.css'
 import NavbarTop from './NavbarTop.js'
 import NavbarBottom from './NavbarBottom.js'
 import List from './List'
-import Card from './Card'
 
 class App extends Component {
   render() {
@@ -12,7 +11,6 @@ class App extends Component {
         <NavbarTop />
         <NavbarBottom />
         <List />
-        <Card />
       </div>
     )
   }
