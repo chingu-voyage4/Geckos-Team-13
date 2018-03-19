7b3df74bf13f2c62b961e52f98e6d9dff468d993
zealousAnemone <zealous.anemone@gmail.com>
2018-03-18 20:20:20 -0400

    Made Follow toggle-able, though all this changes for now is whow is whether list menu says follow or unfollow

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import "../styles/style.css";
 import NavbarTop from "./NavbarTop.js";
 import NavbarBottom from "./NavbarBottom.js";
 import Board from "./Board";
+import ListMenu from "./ListMenu.js";
 
 class App extends Component {
     render() {
@@ -10,6 +11,7 @@ class App extends Component {
             <div className="App">
                 <NavbarTop />
                 <NavbarBottom />
+                <ListMenu />
                 <Board />
             </div>
         );
--- src/components/ListMenu.js
+++ src/components/ListMenu.js
@@ -3,6 +3,21 @@ import "../styles/style.css";
 import "../styles/listmenu.css";
 
 class ListMenu extends React.Component {
+    constructor(props) {
+        super(props);
+        this.toggleFollow = this.toggleFollow.bind(this);
+        this.state = {follow: false};
+    }
+
+    toggleFollow(e) {
+        e.preventDefault();
+        if (this.state.follow === false) {
+            this.setState({follow: true});
+        } else {
+            this.setState({follow: false});
+        }
+    }
+
     render() {
         return (
             <div className="list-menu">
@@ -13,7 +28,7 @@ class ListMenu extends React.Component {
                     <a href="#"><li>Add Card...</li></a>
                     <a href="#"><li>Copy List...</li></a>
                     <a href="#"><li>Move List...</li></a>
-                    <a href="#"><li className="follow">Follow</li></a>
+                    <a href="#"><li className="follow" onClick={this.toggleFollow}>{this.state.follow ? "Unfollow" : "Follow"}</li></a>
                     <a href="#"><li className="sort">Sort By...</li></a>
                     <a href="#"> <li>Move All Cards in This List...</li></a>
                     <a href="#">
