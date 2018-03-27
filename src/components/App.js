f118188a8bca27e19a8f554ddd15f90548582668
zealousAnemone <zealous.anemone@gmail.com>
2018-03-26 22:13:32 -0400

    Simplified color scheme - Removed different background colors for header and replaced with transparent black like on Trello app

--- src/components/App.js
+++ src/components/App.js
@@ -8,31 +8,31 @@ class App extends Component {
     constructor(props) {
         super(props);
 
-        this.state = { color: "#0079bf", navbarColor: "#0067A3"};
+        this.state = { color: "#0079bf"};
         this.changeColor = this.changeColor.bind(this);
     }
 
     changeColor(e) {
         e.preventDefault();
         if (e.target.className === "color-button green") {
-            this.setState({ color: "#519839", navbarColor: "#458131"});
+            this.setState({ color: "#519839"});
         } else if (e.target.className === "color-button blue") {
-            this.setState({color: "#0079bf", navbarColor: "#0067a3"});
+            this.setState({color: "#0079bf"});
         } else if (e.target.className === "color-button red") {
-            this.setState({ color: "#B04632", navbarColor: "#963C2B"});
+            this.setState({ color: "#B04632"});
         } else if (e.target.className === "color-button orange") {
-            this.setState({ color: "#D29034", navbarColor: "#B37B2C"});
+            this.setState({ color: "#D29034"});
         } else if (e.target.className === "color-button purple") {
-            this.setState({ color: "#89609E", navbarColor: "#755286"});
+            this.setState({ color: "#89609E"});
         } else if (e.target.className === "color-button pink") {
-            this.setState({ color: "#CD5A91", navbarColor: "#AE4D7B"});
+            this.setState({ color: "#CD5A91"});
         }
     }
 
     render() {
         return (
             <div className="App" style={{backgroundColor: this.state.color}}>
-                <NavbarTop color={this.state.navbarColor} />
+                <NavbarTop />
                 <NavbarBottom changeColor={this.changeColor} color={this.state.color}/>
                 <Board />
             </div>
