161f14eb4276a3460950266e28019af478673965
Tiffany DeVine <tiffany.devine@icloud.com>
2018-03-28 15:35:43 -0700

    Skeleton for Card preview component. Will display quick edit menu and Card created by faraz

---
+++ src/components/CardPreview.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import QuickEditCard from "./QuickEditCard";
+import Card from "./Card";
+
+export default class  extends Component {
+    render() {
+        return (
+            
+        );
+    }
+}
