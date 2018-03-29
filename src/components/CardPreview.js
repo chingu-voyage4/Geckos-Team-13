5a8da102cf1a5673d07fcc964c1f71720bbeccf0
Tiffany DeVine <tiffany.devine@icloud.com>
2018-03-28 17:59:33 -0700

    base styling and added open card method

--- src/components/CardPreview.js
+++ src/components/CardPreview.js
@@ -2,11 +2,30 @@ import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import QuickEditCard from "./QuickEditCard";
 import Card from "./Card";
+import "../styles/cardpreview.css";
 
-export default class  extends Component {
+export default class extends Component {
+    constructor(props) {
+        super(props);
+
+        state: {
+        }
+
+        this.openCard = this.openCard.bind(this);
+    }
+
+    openCard() {}
+
     render() {
         return (
-            
+            <li className="card-preview">
+                <div onClick={this.openCard} className="card-preview__text">
+                    {this.props.title}
+                </div>
+                <button className="quick-edit-popout" onClick={this.openQuickEdit.bind(this)}>
+                    <i className="fas fa-pencil-alt quick-edit-popout" />
+                </button>
+            </li>
         );
     }
 }
