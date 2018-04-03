15ba6fd92b1c0fec4745e435261ab51d85a55c28
Tiffany DeVine <tiffany.devine@icloud.com>
2018-04-02 18:05:46 -0700

    Added position change method

--- src/components/MoveListSubmenu.js
+++ src/components/MoveListSubmenu.js
@@ -3,6 +3,30 @@ import { connect } from "react-redux";
 import * as actions from "../actions";
 
 class MoveListSubmenu extends Component {
+    //needs listId & position to be passed down from list.
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            selectOpen: false,
+            position: this.props.position,
+            oldpos: null
+        };
+
+        this.positionChange = this.positionChange.bind(this);
+    }
+
+    positionChange() {
+        if (this.state.oldpos >= 0) {
+            const newPosition = this.state.position;
+            const oldpos = this.state.oldpos;
+
+            this.props.moveList(this.props.listId, newPosition, oldpos);
+            //close everything
+        }
+        //close everything
+    }
+
     render() {
         const placeholderBoard = "Placeholder Board";
         return (
