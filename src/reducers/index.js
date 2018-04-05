ca7ea438820e2d9011a1ff37b04306189edb8b4b
Tiffany DeVine <tiffany.devine@icloud.com>
2018-04-04 22:06:49 -0700

    archive as a piece of state

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,12 +3,14 @@ import cardReducer from "./cardReducer";
 import cardArrayReducer from "./cardArrayReducer";
 import listReducer from "./listReducer";
 import listArrayReducer from "./listArrayReducer";
+import archivedReducers from "./archivedReducers";
 
 const rootReducer = combineReducers({
     lists: listReducer,
     cardArray: cardArrayReducer,
     cards: cardReducer,
-    listArray: listArrayReducer
+    listArray: listArrayReducer,
+    archive: archivedReducers
 });
 
 export default rootReducer;
