a54978894d78aec249cccf37f9a0156e2928fcf5
Tiffany DeVine <tiffany.devine@icloud.com>
2018-04-04 22:06:26 -0700

    Added archivedReducer

---
+++ src/reducers/archivedReducers.js
@@ -0,0 +1,20 @@
+import C from "../actions/types";
+
+function archiveList(state, action) {
+    const { payload } = action;
+    const { listId, position } = payload;
+
+    const listItem = { listId, position };
+
+    return [...state, listItem];
+}
+
+export default function(state = {}, action) {
+    switch (action.type) {
+        case C.ARCHIVE_LIST:
+            console.log(archiveList(state, action));
+            return archiveList(state, action);
+        default:
+            return state;
+    }
+}
