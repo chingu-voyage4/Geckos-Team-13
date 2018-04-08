f64adede0c07c1a443f892db7ff05b91a5b60d59
Tiffany DeVine <tiffany.devine@icloud.com>
2018-04-08 13:32:08 -0700

    lists are rendered with a callback to submit move all cards action

--- src/components/MoveAllCardsSubmenu.js
+++ src/components/MoveAllCardsSubmenu.js
@@ -1,7 +1,50 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import * as actions from "../actions";
 
-export default class MoveAllCardsSubmenu extends Component {
+class MoveAllCardsSubmenu extends Component {
+    constructor(props) {
+        super(props);
+
+        this.renderLists = this.renderLists.bind(this);
+        this.test = this.test.bind(this);
+    }
+
+    test(list) {
+        this.props.moveAllCards(this.props.listId, list);
+    }
+
+    renderLists() {
+        const lists = this.props.lists;
+        console.log(lists);
+        return this.props.listArr.map(list => {
+            return (
+                <li
+                    onClick={() => {
+                        this.test(list);
+                    }}
+                    key={list}
+                >
+                    {lists[list].title}
+                </li>
+            );
+        });
+    }
+
     render() {
-        return <div>menu</div>;
+        return (
+            <div>
+                <ul>{this.renderLists()}</ul>
+            </div>
+        );
     }
 }
+
+function mapStateToProps(state) {
+    return {
+        lists: state.lists,
+        listArr: state.listArray
+    };
+}
+
+export default connect(mapStateToProps, actions)(MoveAllCardsSubmenu);
