61a37d4cc4f5679b9b876328efa633358143efbe
Tiffany DeVine <tiffany.devine@icloud.com>
2018-04-08 23:18:12 -0700

    Added a functional archive submenu option to more menu

--- src/components/MoreMenu.js
+++ src/components/MoreMenu.js
@@ -21,39 +21,51 @@ export default class MoreMenu extends Component {
 
     showSubmenu(e) {
         e.preventDefault();
-        if (e.target.className === "change-background") {
-            this.setState({ menu: "backgroundMenu", menuTitle: "Change Background" });
-        } else if (e.target.className === "more-menu") {
-            this.setState({ menu: "moreMenu", menuTitle: "More" });
+        if (e.target.className === "archive") {
+            this.setState({ menu: "archiveMenu", menuTitle: "Archive" });
         }
     }
     render() {
-        return (
-            <div className="board-menu">
-                <div className="board-menu-title">
-                    <span className="back" onClick={this.props.showMainMenu}>
-                        <i className="fas fa-arrow-left" />
-                    </span>
-                    <h3>{this.props.menuTitle}</h3>
-                    <span className="close" onClick={this.props.toggleBoardMenu}>
-                        <img src="../close-round.png" alt="close" />
-                    </span>
+        if (this.state.menu === "main") {
+            return (
+                <div className="board-menu">
+                    <div className="board-menu-title">
+                        <span className="back" onClick={this.props.showMainMenu}>
+                            <i className="fas fa-arrow-left" />
+                        </span>
+                        <h3>{this.props.menuTitle}</h3>
+                        <span className="close" onClick={this.props.toggleBoardMenu}>
+                            <img src="../close-round.png" alt="close" />
+                        </span>
+                    </div>
+                    <div className="board-options">
+                        <li>
+                            <i className="fas fa-cog" />Settings
+                        </li>
+                        <li>
+                            <i className="fas fa-tag" />Labels
+                        </li>
+                        <li>
+                            <i className="far fa-folder-open" />Collections
+                        </li>
+                        <li className="archive" onClick={this.showSubmenu}>
+                            <i className="fas fa-archive" />Archived Items
+                        </li>
+                    </div>
                 </div>
-                <div className="board-options">
-                    <li>
-                        <i className="fas fa-cog" />Settings
-                    </li>
-                    <li>
-                        <i className="fas fa-tag" />Labels
-                    </li>
-                    <li>
-                        <i className="far fa-folder-open" />Collections
-                    </li>
-                    <li className="more-menu">
-                        <i className="fas fa-archive" />Archived Items
-                    </li>
+            );
+        } else {
+            return (
+                <div>
+                    {this.state.menu === "archiveMenu" && (
+                        <ArchiveMenu
+                            toggleBoardMenu={this.props.toggleBoardMenu}
+                            showMainMenu={this.showMainMenu}
+                            menuTitle={this.state.menuTitle}
+                        />
+                    )}
                 </div>
-            </div>
-        );
+            );
+        }
     }
 }
