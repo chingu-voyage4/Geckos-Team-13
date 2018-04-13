1faaffdc071e83f68c8532a9dc1d3d76dcde6dd5
Tiffany DeVine <tiffany.devine@icloud.com>
2018-04-13 15:47:32 -0700

    created reducer to update the listId within the archived card array reducer

--- src/reducers/archivedCardsReducer.js
+++ src/reducers/archivedCardsReducer.js
@@ -20,6 +20,27 @@ function removeCard(state, action) {
     return state.filter(item => item.cardId !== cardId);
 }
 
+function updateOneItem(state, action) {
+    const { cardId, selectedList, position } = action.payload;
+
+    const updated = state.filter(item => item.cardId !== cardId);
+    updated.listId = selectedList;
+    updated.position = position;
+
+    return state.map(item => {
+        if (item.cardId !== cardId) {
+            return item;
+        }
+
+        item.listId = selectedList;
+
+        return {
+            ...item,
+            ...updated
+        };
+    });
+}
+
 export default function(state = [], action) {
     switch (action.type) {
         case C.ARCHIVE_ALL_CARDS:
@@ -28,6 +49,8 @@ export default function(state = [], action) {
             return archiveCard(state, action);
         case C.RESTORE_CARD:
             return removeCard(state, action);
+        case C.UPDATE_ARCHIVED_CARD:
+            return updateOneItem(state, action);
         default:
             return state;
     }
